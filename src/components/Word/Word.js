99ea78792db3e01b816e2574ccb053c5f7418182
Jamey <nakamajamey+git@gmail.com>
2021-08-12 19:43:57 -0700

    Clean up punctuation

--- src/components/Word/Word.js
+++ src/components/Word/Word.js
@@ -1,15 +1,19 @@
 import React from "react";
 import PropTypes from "prop-types";
+import classnames from 'classnames';
 
 import css from "./Word.module.css";
 
+const PUNCTUATION = [ ".", ",", ";" ];
+
 const Word = ({ word, onClick }) => {
+  const classes = classnames(PUNCTUATION.includes(word) && css.punctuation);
   return (
     <span className={css.wrapper}>
       {onClick ?
-        <button onClick={onClick}>{word}</button>
+        <button className={classes} onClick={onClick}>{word}</button>
         :
-        <span>{word}</span>
+        <span className={classes}>{word}</span>
       }
     </span>
   );
